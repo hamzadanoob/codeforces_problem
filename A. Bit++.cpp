540bf36e4c89626a08d8e798980ed5c966f7d238
Hamza Bin Arif <hamzabinarif.arif@gmail.com>
2025-02-21 09:53:52 +0600

    Add files via upload

---
+++ A. Bit++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x = 0;
+    cin >> n;
+
+    while (n--) {
+        string s;
+        cin >> s;
+        if (s == "X++" || s == "++X") x++;
+        else x--;
+    }
+
+    cout << x << endl;
+    return 0;
+}
